1a2c48a413ca11d2a23414e601964711fe3b5f9e
sidpav1980-alt <sid.pav1980@gmail.com>
2026-08-15 01:44:55 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@ const CACHE='trail-analyzer-web-v098';
 const CORE=[
   './',
   './index.html',
-  './styles.css?v=235',
-  './app.js?v=235',
+  './styles.css?v=240levels',
+  './app.js?v=240levels',
   './manifest.webmanifest?v=075',
   './icon-192.png',
   './icon-512.png'
